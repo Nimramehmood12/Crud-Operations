9d5217912b92557f7e5d77c4e0b7d8b32757c791
Nimramehmood12 <mehmoodnimra773@gmail.com>
2025-08-02 16:22:42 +0500

    Manually added all project files

---
+++ client/src/Users.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { useState } from "react";
+import "./Users.css";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useEffect } from "react";
+
+function Users() {
+  const [users, setUsers] = useState([]);
+  useEffect(()=>{
+     axios.get('http://localhost:8000')
+     .then(result=>setUsers(result.data))
+     .catch(err=>console.error(err))
+
+  },[])
+
+  const handleDelete = (id) => {
+    axios.delete(`http://localhost:8000/deleteUser/`+id)
+      .then(res => {console.log(res)
+        window.location.reload()
+      })
+      .catch(err => console.error(err));
+    }
+  return (
+    <div className="users-bg">
+      <div className="users-card">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-start",
+            marginBottom: "0.8rem",
+          }}
+        >
+          <Link to="/createUser" className="btn ">
+            Add +
+          </Link>
+        </div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Age</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => {
+              return (
+                <tr>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.age}</td>
+                  <td>
+                    <div style={{ display: "flex", gap: "0.5rem" }}>
+                      <Link to={`/update/${user._id}`} className="btn">
+                        Update
+                      </Link>
+                      <button className='btn btn-danger'
+                       onClick={(e)=>handleDelete(user._id)}>Delete</button>
+                    </div>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default Users;
